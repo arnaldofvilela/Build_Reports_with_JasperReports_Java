788c211ac11ce180e4e6c93ca529c2fb19811c8a
Arnaldo Neto <vilela-neto@hotmail.com>
2022-11-02 16:48:03 -0300

    011-Generate Report In PDF Format

--- src/main/java/com/infybuzz/report/FirstReport.java
+++ src/main/java/com/infybuzz/report/FirstReport.java
@@ -1,5 +1,6 @@
 package com.infybuzz.report;
 
+import net.sf.jasperreports.engine.*;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 
 import java.util.ArrayList;
@@ -18,13 +19,21 @@ public class FirstReport {
 			parameters.put("studentName", "John");
 
 			Student student1 = new Student(1L, "Raj", "Joshi", "Happy Street", "Delhi");
-			Student student2 = new Student(1L, "Peter", "Smith", "Any Street", "Mumbai");
+			Student student2 = new Student(2L, "Peter", "Smith", "Any Street", "Mumbai");
 
 			List<Student> list = new ArrayList<Student>();
 			list.add(student1);
 			list.add(student2);
 
 			JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(list);
+
+			JasperReport report = JasperCompileManager.compileReport(filePath);
+
+			JasperPrint print = JasperFillManager.fillReport(report, parameters, dataSource);
+
+			JasperExportManager.exportReportToPdfFile(print, "/home/arnaldo/Desktop/Exported-Reports/FirstReport.pdf");
+//			JasperExportManager.exportReportToPdf(print);
+			System.out.println("Report Created...");
 
 		} catch(Exception e) {
 			System.out.println("Exception while creating report");
